b4330c7082c9c5df53f277598b8c9bcb729e7258
Benjamin Prince <princebe@os1.engr.oregonstate.edu>
2021-05-20 16:39:31 -0700

    Initial Commit

---
+++ enc_server.c
@@ -0,0 +1,19 @@
+/*********************************************************************
+ *      Author: Ben Prince
+ *      Assignment 5: One-Time Pad
+ *      Date: 05/31/2021
+ * 
+ *      Encryption Server File
+ * ******************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+
+    
+
+    return 0;
+}
