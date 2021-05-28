17b504f6ef8ae649abd624d3eee2f9ec4d0c70b7
Benjamin Prince <princebe@os1.engr.oregonstate.edu>
2021-05-28 07:02:18 -0700

    decryption set up

--- dec_client.c
+++ dec_client.c
@@ -4,16 +4,183 @@
  *      Date: 05/31/2021
  * 
  *      Decryption Client File
+ *      Source: https://www.youtube.com/watch?v=9g_nMNJhRVk&list=PLPyaR5G9aNDvs6TtdpLcVO43_jvxp4emI&index=8
  * ******************************************************************/
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>  // ssize_t
+#include <sys/socket.h> // send(),recv()
+#include <netdb.h>      // gethostbyname()
+
+// Error function used for reporting issues
+// Design copied from client.c example file on assignment page
+void error(const char *msg) {
+/******************************************************************
+*  param: char * 
+*  return: NA
+*  description: Prints error message and exits
+* ***************************************************************/
+
+    perror(msg);
+    exit(0);
+
+}
+
+// Set up the address struct
+void setupAddressStruct(struct sockaddr_in* address, int portNumber){
+
+    // Clear out the address struct
+    memset((char*) address, '\0', sizeof(*address)); 
+
+    // The address should be network capable
+    address->sin_family = AF_INET;
+    // Store the port number
+    address->sin_port = htons(portNumber);
+
+    // Get the DNS entry for this host name
+    struct hostent* hostInfo = gethostbyname("localhost"); 
+    if (hostInfo == NULL) { 
+    fprintf(stderr, "CLIENT: ERROR, no such host\n"); 
+    exit(0); 
+    }
+    // Copy the first IP address from the DNS entry to sin_addr.s_addr
+    memcpy((char*) &address->sin_addr.s_addr, 
+        hostInfo->h_addr_list[0],
+        hostInfo->h_length);
+
+}
 
 int main(int argc, char *argv[]) {
 
-    
+    // Initial Setup
+    int socketFD, portNumber, charsWritten, charsRead, fileLength, keyLength;
+    struct sockaddr_in serverAddress;
+    FILE *plainText;
+    FILE *key;   // was key
+    char buffer[256];   // change to 1024
+    int size = 256;
+    char sendText[100000];
+    char keyText[100000];
+    char readText[100000];
 
+    // Check usage & args
+    if (argc < 4) { 
+        fprintf(stderr,"USAGE: %s hostname port\n", argv[0]);
+        fprintf(stderr,"Example: enc_client plaintext key port\n");
+        exit(0); 
+    } 
+
+    // Create a socket
+    socketFD = socket(AF_INET, SOCK_STREAM, 0); 
+    if (socketFD < 0){
+
+        error("CLIENT: ERROR opening socket");
+
+    }
+
+    // Set up the server address struct
+    setupAddressStruct(&serverAddress, atoi(argv[3]));
+
+    // Connect to server
+    if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0){
+
+        error("CLIENT: ERROR connecting");
+
+    }
+    
+    //Send text to server
+    plainText = fopen(argv[1], "r");
+    if (plainText == NULL) {
+
+        error("CLIENT: Couldn't find plain text file");
+
+    }
+
+    key = fopen(argv[2], "r");
+    if (key == NULL) {
+
+        error("CLIENT: Couldn't find key file");
+
+    }
+
+    memset(buffer, '\0', sizeof(buffer));
+
+    char c;
+    fileLength = 0;
+    for(c = getc(plainText); c != EOF; c = getc(plainText)) {
+
+        sendText[fileLength] = c;
+        fileLength++;
+
+    }
+    sendText[strcspn(sendText, "\n")] = '\0';
+    sendText[strcspn(sendText, "\n")] = '@';
+
+    keyLength = 0;
+    int tempLength = fileLength;
+    while((c = getc(key)) != EOF) {
+        
+        sendText[fileLength] = c;
+        fileLength++;
+        keyLength++;
+
+        if(keyLength > tempLength) {
+            break;
+        }
+
+    }
+    if(keyLength < (fileLength - keyLength)) {
+        
+        error("CLIENT Error: Key is too short");
+
+    }
+
+    sendText[strcspn(sendText, "\n")] = '\0';
+    keyLength--;
+
+    write(socketFD, &fileLength, sizeof(int));
+    rewind(plainText);
+    rewind(key);
+
+    char ch;
+    charsWritten = 0;
+    while(charsWritten <= fileLength) {  // Possibly change this
+
+        charsWritten += write(socketFD, sendText, 1000);
+
+    } 
+
+    fclose(plainText);
+    fclose(key);
+
+    // Get return message from server
+    // Clear out the buffer again for reuse
+    memset(buffer, '\0', sizeof(buffer));
+
+    // Read data from the socket, leaving \0 at end
+    charsRead = 0;
+    while(charsRead < fileLength) {
+
+        charsRead += recv(socketFD, buffer, 10, 0);
+        strcat(readText, buffer);
+        memset(buffer, '\0', sizeof(buffer));
+
+    }
+    
+    if (charsRead < 0){
+
+        error("CLIENT: ERROR reading from socket");
+
+    }
+
+    // Send encrypted text to stdout
+    printf("%s", readText);
+    fflush(stdout);
+
+    // Close the socket
+    close(socketFD); 
     return 0;
 }
--- dec_server.c
+++ dec_server.c
@@ -4,16 +4,196 @@
  *      Date: 05/31/2021
  * 
  *      Decryption Server File
+ *      Source: https://www.youtube.com/watch?v=9g_nMNJhRVk&list=PLPyaR5G9aNDvs6TtdpLcVO43_jvxp4emI&index=8
  * ******************************************************************/
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>  // ssize_t
+#include <sys/socket.h> // send(),recv()
+#include <sys/wait.h>   // waitpid()
+#include <netdb.h>      // gethostbyname()
+
+// Error function used for reporting issues
+// Design copied from server.c example file on assignment page
+void error(const char *msg) {
+/******************************************************************
+ *  param: char * 
+ *  return: NA
+ *  description: Prints error message and exits
+ * ***************************************************************/
+    perror(msg);
+    exit(0);
+
+}
+
+void decrypt(char *text, char *key, char *decryptedText) {
+
+    int i;
+    int textChar;
+    int keyChar;
+    int newChar;
+
+    for(i=0; i < strlen(text); i++) {
+
+        // Convert text chars to int 
+        if(text[i] == ' ') {
+            textChar = 26;
+        } else {
+            textChar = text[i] - 'A';
+        }
+
+        // Convert key chars to int
+        if(key[i] == ' ') {
+            keyChar = 26;
+        } else {
+            keyChar = key[i] - 'A';
+        }
+
+        // Get decrypted character int
+        newChar = textChar - keyChar;
+        if(newChar < 0) {
+            newChar += 27;
+        }
+        // Convert decrypted character int to a char value
+        if(newChar == 26) {
+            decryptedText[i] = ' ';
+        } else {
+            decryptedText[i] = newChar + 'A';
+        }
+    }
+    // Add new line char at the end
+    decryptedText[i] = '\n';
+}
+
+// Set up the address struct for the server socket
+void setupAddressStruct(struct sockaddr_in* address, 
+                        int portNumber){
+ 
+  // Clear out the address struct
+  memset((char*) address, '\0', sizeof(*address)); 
+
+  // The address should be network capable
+  address->sin_family = AF_INET;
+  // Store the port number
+  address->sin_port = htons(portNumber);
+  // Allow a client at any address to connect to this server
+  address->sin_addr.s_addr = INADDR_ANY;
+}
 
 int main(int argc, char *argv[]) {
 
+    pid_t spawnpid = -5;
+    int connectionSocket, charsRead, childStatus;
+    struct sockaddr_in serverAddress, clientAddress;
+    socklen_t sizeOfClientInfo = sizeof(clientAddress); 
     
+    // Check arguments
+    if (argc < 2) { 
+        fprintf(stderr,"SERVER: Argument Error\n"); 
+        exit(1);
+    }
 
+    // Create the socket that will listen for connections
+    int listenSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenSocket < 0) {
+        error("ERROR opening socket");
+    }
+
+    // Set up the address struct for the server socket
+    setupAddressStruct(&serverAddress, atoi(argv[1]));
+
+    // Associate the socket to the port
+    if (bind(listenSocket, 
+        (struct sockaddr *)&serverAddress, 
+        sizeof(serverAddress)) < 0){
+            error("ERROR on binding");
+    }
+
+    // Start listening for connetions. Allow up to 5 connections to queue up
+    listen(listenSocket, 5); 
+
+      // Accept a connection, blocking if one is not available until one connects
+    while(1){
+
+        // Accept the connection request which creates a connection socket
+        connectionSocket = accept(listenSocket, 
+                    (struct sockaddr *)&clientAddress, 
+                    &sizeOfClientInfo); 
+        if (connectionSocket < 0){
+            error("ERROR on accept");
+        }
+
+        spawnpid = fork();
+        switch(spawnpid) {
+            case -1: {
+
+                error("SERVER: fork() failed");
+                exit(1);
+                break;
+            }
+            
+            case 0: {
+
+                char buffer[1024];
+                char fullText[100000];
+                char parsedKey[100000];
+                char parsedText[100000];
+                char decryptedText[100000];
+                char *key;
+                char *text;
+                int fileLength, charsRead;
+
+                //Read number of chars to expect
+                read(connectionSocket, &fileLength, sizeof(int));
+
+                // Read chars and set them in fullText
+                charsRead = 0;
+                while(charsRead <= fileLength) {
+
+                    charsRead += recv(connectionSocket, buffer, 255, 0);
+                    strcat(fullText, buffer);
+                    memset(buffer, '\0', 1024);
+
+                }
+                if (charsRead < 0){
+                    error("ERROR reading from socket");
+                }
+
+                // text and key are separated by a @ char. strtok to split them.
+                text = strtok(fullText, "@");
+                strcpy(parsedText, text);
+                key = strtok(NULL, "@");
+                strcpy(parsedKey, key);
+
+                // Call decrypt to put the decrypted text in decryptedText string
+                decrypt(parsedText, parsedKey, decryptedText);
+
+                // Send decrypted text back to client
+                charsRead = 0;
+                while(charsRead < strlen(decryptedText)) {
+                    charsRead += send(connectionSocket, decryptedText, 1000, 0);
+                }
+                if (charsRead < 0){
+                    error("ERROR writing to socket");
+                }
+
+                // Close the connection socket for this client
+                close(connectionSocket);
+                break;
+
+            }
+            default: {
+
+                spawnpid = waitpid(spawnpid, &childStatus, WNOHANG);
+            }
+        }
+    }
+
+    // Close the listening socket
+    close(listenSocket); 
     return 0;
+
 }
